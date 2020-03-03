cc726417fc582f5de4636cffef18a4d3d63eee69
MohammadRezaMahmoudi <akoweb@users.noreply.github.com>
2020-03-03 23:45:28 +0330

    index.php
    put index.php  in the your directory...
    move all files from other servers...
    
    >> small and safe! <<
    >> with benchmark & files list and file size - create custoemr file name!
    >> work with php > 5.5 to upper
    >> need to allow_url_fopen (file_get_contents + file_put_content) in php configuration
    *** after move your files , remove index.php

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+set_time_limit(0);
+ini_set('memory_limit','2048M'); 
+?>
+<style>
+*.html{ padding:0px; margin:0px;}
+body{ margin:15px auto; width:90%; font-family:tahoma; font-size:12px; line-height:199%;}
+input{ width:100%; margin:15px; padding:15px; border:1px solid #ccc; cursor:pointer}
+table td{ text-align:center}
+a, a:link, a:visited{ color:#009; text-decoration:underline}
+</style>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>  PHP Files Transfer  </title>
+</head>
+
+<body>
+
+<form method="post" action="" enctype="multipart/form-data">
+
+<input type="text" dir="ltr" placeholder="file url : http://www.ako.ir" required="required" autofocus="autofocus" name="fileurl" size="150" /><br/>
+<input type="text" dir="ltr" placeholder="file name : mrm.zip" required="required" autofocus="autofocus" name="filename" size="150" /><br/>
+<input type="submit" value="submit" />
+</form>
+
+
+
+
+<?php
+
+
+echo 'لینک مبدا : '.urldecode($_POST['fileurl']).'<br/>';
+echo 'فایل مقصد : '.urldecode($_POST['filename']).'<br/><br/>';
+
+if( !empty( $_POST['fileurl'] )){
+	
+	echo  'strat date : '. date('Y-m-d H:i:sa').'<br/>';
+	
+	$file = urldecode(trim($_POST['fileurl']));
+	
+	$file_name =  urldecode(trim($_POST['filename']));
+	
+	if( file_put_contents( $file_name, file_get_contents( $file ) ) ){
+		
+		echo '<b>file moved!</b><br/>';
+		
+		}
+	 
+	
+	echo  ' end date : '.date('Y-m-d H:i:sa').'<br/>';
+
+}
+
+
+
+?>
+<hr/>
+File's List
+
+<table cellpadding="5" cellspacing="5" border="2" width="100%">
+<tr><th>#</th><th>name/download</th><th>Size</th></tr>
+
+<?php
+
+ $dir = opendir("./");
+ $i = 1;
+ 
+ while($myfile = readdir($dir)){
+	 
+	 
+	 if($myfile != "index.php" and $myfile != "." and $myfile != ".."){
+	 echo '
+	 <tr> <td>'.$i.' </td>
+	 <td><a target="_blank" href="./'. $myfile .'">'.$myfile.'</a></td>
+	 <td>'. round(  (filesize($myfile)/1024)/1024 ).' MB </td></tr>';
+	 
+	 $i++;
+	 
+	 }
+	 
+	 
+	 
+	 }
+
+
+
+
+?>
+
+</table>
+
+</body>
+</html>
